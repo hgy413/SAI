205b400b1ed38145d9794e87821cb62d67f0e424
huangguoyou <huangguoyou@yy.com>
2021-08-20 11:59:51 +0800

    去掉无用的代码

--- app/src/main/java/com/mcool/sai/utils/PreferencesKeys.java
+++ app/src/main/java/com/mcool/sai/utils/PreferencesKeys.java
@@ -2,11 +2,7 @@ package com.mcool.sai.utils;
 
 public class PreferencesKeys {
     public static final String MIUI_WARNING_SHOWN = "miui_warning_shown";
-    public static final String INSTALLER = "installer";
-    public static final String INSTALL_LOCATION = "install_location";
-    public static final String SHOW_INSTALLER_DIALOGS = "show_installer_dialogs";
     public static final String SHOW_APP_FEATURES = "show_app_features";
-    public static final String USE_INSTALLERX = "use_installerx";
     public static final String USE_BRUTE_PARSER = "use_brute_parser";
     public static final String INITIAL_INDEXING_RUN = "initial_indexing_run";
     public static final String BACKUP_APK_EXPORT = "single_apk_export";
--- app/src/main/java/com/mcool/sai/utils/PreferencesValues.java
+++ app/src/main/java/com/mcool/sai/utils/PreferencesValues.java
@@ -3,8 +3,6 @@ package com.mcool.sai.utils;
 public class PreferencesValues {
 
     public static final int INSTALLER_ROOTLESS = 0;
-    public static final int INSTALLER_ROOTED = 1;
-    public static final int INSTALLER_SHIZUKU = 2;
 
 
 }
